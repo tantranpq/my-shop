0daef64643b7c42781b22826f842dae47d2c3bfd
tantranpq <quangtan.pq@gmail.com>
2025-05-20 09:59:34 +0700

    Sửa lỗi ESLint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,11 @@
 // app/layout.tsx
 "use client";
 import "./globals.css";
-import { useCart, CartProvider } from "../context/CartContext";
+import { CartProvider } from "../context/CartContext";
 import { supabase } from "../lib/supabase";
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { useState } from 'react';
-
-const Notification = () => {
-  const { notification } = useCart();
-
-  if (notification) {
-    return (
-<div
-        className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white py-2 px-4 rounded-md shadow-md z-50"
-        style={{ zIndex: 100 }} // Thêm inline style để tăng z-index
-      >
-        {notification}
-      </div>
-    );
-  }
-  return null;
-};
+import Notification from '../app/notification'; 
 
 export default function RootLayout({
   children,

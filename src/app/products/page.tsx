605c923bed78c9501b68eeea65f8071b342c40e8
tantranpq <quangtan.pq@gmail.com>
2025-05-20 11:06:51 +0700

    Thử trang product không có search

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -7,14 +7,9 @@ import ProductList from "@/components/ProductsList";
 import { supabase } from "@/lib/supabase";
 import { Product } from "@/types/product";
 import React from "react";
-import { useSearchParams } from 'next/navigation';
 
-async function getProducts(searchTerm: string | null): Promise<Product[]> {
-    let query = supabase.from("products").select("*");
-    if (searchTerm) {
-        query = query.ilike('name', `%${searchTerm}%`); // Tìm kiếm gần đúng theo tên
-    }
-    const { data: products, error } = await query;
+async function getProducts(): Promise<Product[]> {
+    const { data: products, error } = await supabase.from("products").select("*");
     if (error) {
         console.error("Lỗi khi tải sản phẩm:", error);
         return [];
@@ -24,8 +19,6 @@ async function getProducts(searchTerm: string | null): Promise<Product[]> {
 
 export default function ProductsPage() {
     const { addToCart } = useCart();
-    const searchParams = useSearchParams();
-    const searchTerm = searchParams.get('search');
     const [products, setProducts] = React.useState<Product[]>([]);
     const [loading, setLoading] = React.useState(true);
     const [error, setError] = React.useState<Error | null>(null);
@@ -33,7 +26,7 @@ export default function ProductsPage() {
     React.useEffect(() => {
         async function fetchProducts() {
             try {
-                const data = await getProducts(searchTerm);
+                const data = await getProducts();
                 setProducts(data);
                 setLoading(false);
             } catch (err: unknown) {
@@ -47,7 +40,7 @@ export default function ProductsPage() {
         }
 
         fetchProducts();
-    }, [searchTerm]);
+    }, []);
 
     const handleAddToCart = (product: Product) => {
         addToCart(product);
@@ -66,13 +59,12 @@ export default function ProductsPage() {
         <>
             <Navbar />
             <main className="max-w-6xl mx-auto p-6">
-                <h1 className="text-2xl font-bold mb-6">
-                    {searchTerm ? `Kết quả tìm kiếm cho &quot;${searchTerm}&quot;` : 'Sản phẩm'}
-                </h1>
+                <h1 className="text-2xl font-bold mb-6">Sản phẩm</h1>
                 {products && products.length > 0 ? (
                     <ProductList products={products} onAddToCart={handleAddToCart} />
                 ) : (
-                    searchTerm && <p>Không tìm thấy sản phẩm nào phù hợp với từ khóa &quot;{searchTerm}&quot;.</p>)}
+                    <p>Không có sản phẩm nào.</p>
+                )}
             </main>
         </>
     );
